9dccfbb2b42c34a60ef8060c1bed163a68220a4c
YitGubTheSecond <95759371+YitGubTheSecond@users.noreply.github.com>
2025-04-19 19:33:03 +0000

    Added saving checkmarks, themes, and fixed bugs.

--- processing.js
+++ processing.js
@@ -5,24 +5,70 @@ function activate(id) {
       localStorage.setItem(id, false)
    }
 }
+function changetheme() {
+   const currenttheme = localStorage.getItem("theme");
+   if (currenttheme == "dark") {
+      localStorage.setItem("theme", "light")
+      const allElements = document.querySelectorAll('*')
+      allElements.forEach(element => {
+         element.style.color = "black"; 
+      });
+      document.body.style.backgroundColor = "white"
+      document.getElementById("theme").style.color = 'rgb(64, 64, 255)';
+   } else {
+      localStorage.setItem("theme", "dark")
+      const allElements = document.querySelectorAll('*')
+      allElements.forEach(element => {
+         element.style.color = "white"; 
+      });
+      document.body.style.backgroundColor = "black"
+      document.getElementById("theme").style.color = 'rgb(64, 64, 255)';
+   }
+}
 async function load() {
+   const currenttheme = localStorage.getItem("theme");
+   if (currenttheme == "dark") {
+      const allElements = document.querySelectorAll('*')
+      allElements.forEach(element => {
+         element.style.color = "white"; 
+      });
+      document.body.style.backgroundColor = "black"
+      document.getElementById("theme").style.color = 'rgb(64, 64, 255)';
+   } else {
+      const allElements = document.querySelectorAll('*')
+      allElements.forEach(element => {
+         element.style.color = "black"; 
+      });
+      document.body.style.backgroundColor = "white"
+      document.getElementById("theme").style.color = 'rgb(64, 64, 255)';
+   }
    let id = 0;
    const res = await fetch('./data.json');
    const json = await res.json();
    const serieslength = (json.children.length) - 1;
    for (let s = 0; s <= serieslength; s++) {
-      document.getElementById("container").innerHTML += "<h2>[" + json.children[s].name + "]</h3>";
+      // document.getElementById("container").innerHTML += "<h2>[" + json.children[s].name + "]</h3>";
+      const header = document.createElement("label");
+      header.innerHTML = "<h2>[" + json.children[s].name + "]</h2>";
+      document.body.appendChild(header);
       const length = await (json.children[s].children.length) - 1;
       for (let i = 0; i <= length; i++) {
-         console.log(localStorage.getItem(id))
-         console.log(id.toString())
-         document.getElementById("container").innerHTML += "<h3>" + json.children[s].children[i].name + "</h3><input type='checkbox' onclick='activate(" + id + ")' id=" + id.toString() + ">";
+         const checkbox = document.createElement("input");
+         const label = document.createElement("label");
+         const div = document.createElement("div");
+         checkbox.type = "checkbox";
+         checkbox.id = id.toString();
+         label.innerHTML = "<h3 style='display:inline-block;'>" + json.children[s].children[i].name + "</h3>";
+         label.appendChild(checkbox);
+         div.appendChild(label);
+         header.appendChild(div);
+         label.style.display = "inline-block";
+         checkbox.style.display = "inline-block";
          if (localStorage.getItem(id) == "true"){
-            console.log(document.getElementById(id))
-            document.getElementById(id.toString()).checked = true;
+            checkbox.checked = true;
          }
-         if (document.getElementById(id).checked == true){
-            console.log("RAHHHH")
+         checkbox.onclick = function() {
+            activate(checkbox.id);
          }
          id += 1;
       }
